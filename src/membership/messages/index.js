eac9ed92d7db2f13e42fcef036d5c72864532acf
Chris Rose <offline@offby1.net>
2018-07-28 07:34:06 -0700

    Import all of our messages

--- src/membership/messages/index.js
+++ src/membership/messages/index.js
@@ -1,13 +1,5 @@
-import en from './en.messages.json'
-import daypass from './daypass.messages.json'
-import fi from './fi.messages.json'
-
 export default {
-  // daypass: require('messageformat?locale=en!json-loader!./daypass.json'),
-  // en: require('messageformat?locale=en!json-loader!./en.json'),
-  // fi: require('messageformat?locale=fi!json-loader!./fi.json')
-  // daypass: require('./daypass.messages.json'),
-  // en: require('./en.messages.json'),
-  // fi: require('./fi.messages.json')
-  daypass: daypass
+  daypass: require('./daypass.messages.json'),
+  en: require('./en.messages.json'),
+  fi: require('./fi.messages.json')
 }
